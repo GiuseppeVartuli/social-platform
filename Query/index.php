2a9a1a391972f6815c1d65da8c43be4007e27aa1
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-04-11 16:59:17 +0200

    printed query data on page

--- Query/index.php
+++ Query/index.php
@@ -12,12 +12,11 @@ if ($connection && $connection->connect_error) {
   }
 
 
-  $query = "SELECT `users`.`username` AS `uploaded_video`
+  $query = "SELECT `users`.`id`,`users`.`username`, COUNT(*) AS 'uploaded_video' 
   FROM `medias`
-  JOIN `users` 
-  ON `users`.`id` = `medias`.`user_id`
+  JOIN `users` ON `users`.`id` = `medias`.`user_id`
   WHERE `medias`.`type` = 'video'
-  GROUP BY `users`.`username`;";
+  GROUP BY `users`.`username`, `users`.`id`;";
   $results = $connection->query($query);
   // var_dump($results);
 
@@ -29,18 +28,57 @@ if ($connection && $connection->connect_error) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-<?php
-  if ($results->num_rows > 0) :
+
+
+  
+    <div class="container">
+      <div class="row">
+      <div class="col-6">
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Utenti</th>
+      <th scope="col">Video Caricati</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+      if ($results->num_rows > 0) :
     while ($row = $results->fetch_assoc()) : ?>
-
-
-      <div><?php echo $row['uploaded_video'] ?></div>
-
-
-  <?php endwhile;
+    <tr>
+      
+      <th scope="row"><?php echo $row['id'] ?></th>
+      <td><?php echo $row['username'] ?></td>
+      <td><?php echo $row['uploaded_video'] ?></td>
+       
+    </tr>
+    <?php endwhile;
   endif;  ?>
+  </tbody>
+</table>
+      </div>
+      </div>
+    </div>
+      
+ 
 </body>
 </html>
+
+
+
+
+<!--
+
+<div class="container">
+      
+        <div class="row">
+          <div><?php echo $row['uploaded_video'] ?></div>
+        </div>
+      </div>
+    
+    -->
